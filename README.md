8ca3776eafb149792b974a14952b2ba6b016efeb
Brian Shaughnessy <brian@lcdservices.biz>
2018-06-28 17:12:42 -0400

    add log tracking table and error logging options

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ After installing the extension through the normal means, navigate to Administer 
 
 Now navigate to Administer > System Settings > Scheduled Jobs and locate the Log Retention job. Edit the job, enable it, and set it to the desired frequency. Because the job is potentially long running, we suggest you run it weekly and schedule it for the weekend (assuming that is a low traffic time for your site).
 
+Additional optional parameteres that can be passed to the API:
+
+* limit = integer: Limits how many records per table will be processed per job run.
+* logoutput = boolean: If set to 1, details about the purging process will be logged to a CiviCRM log file with a "logretention" prefix.
+
+Because this can be a long running job you may find the script will timeout or exceed allowed memory consumption before completing, especially if the first time you run it involves purging a large number of records. As the script cycles through tables and rows it logs progress and can pick up where it left off within a 1-day period. If you find the script terminates prematurely you may want to set the limit parameter and run the job every day until it catches up with the full purging process.
+
 <b>Note: </b>The log purging/retention process can only be performed on logging tables configured using the InnoDB or MyISAM table engine type. The default engine used by the enhanced logging feature in CiviCRM is ARCHIVE, which does not support row deletion. If you have installed this extension before enabling enhanced logging, it will create your tables using InnoDB. If your logging tables have already been created, enable this extension and then run the system.updatelogtables API function which will rebuild those tables using InnoDB as defined in this extension.
--- api/v3/Job/Logretention.php
+++ api/v3/Job/Logretention.php
@@ -8,6 +8,14 @@
  * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
  */
 function _civicrm_api3_job_logretention_spec(&$params) {
+  $params['limit'] = array(
+    'title' => 'Limit',
+    'description' => 'Limit how many rows per table are processed.',
+  );
+  $params['logoutput'] = array(
+    'title' => 'Log Output',
+    'description' => 'If set to 1, details about the log purging process will be logged to a CiviCRM logging file with the logretention prefix. This can be useful for debugging and to track progress.',
+  );
 }
 
 /**
@@ -23,6 +31,11 @@ function civicrm_api3_job_logretention($params) {
   if (!CRM_Core_Config::singleton()->logging) {
     return civicrm_api3_create_error('Logging must be enabled in order to use this API.');
   }
+
+  if (CRM_Utils_Array::value('logoutput', $params)) {
+    define('LOGOUTPUT', TRUE);
+  }
+  _logOutput('params', $params);
   
   $dsn = defined('CIVICRM_LOGGING_DSN') ? DB::parseDSN(CIVICRM_LOGGING_DSN) : DB::parseDSN(CIVICRM_DSN);
   $loggingDB = $dsn['database']; //logging database
@@ -54,25 +67,66 @@ function civicrm_api3_job_logretention($params) {
   }
   $logTables = $logTables + $customTables;
   
-  $params = array(
+  $paramsCommon = array(
     1 => array($ages_ago, 'String'),
   );
-  //Civi::log()->debug('logretention', ['params' => $params]);
+  //Civi::log()->debug('logretention', ['$paramsCommon' => $paramsCommon]);
 
   //get tables excluded from log retention
   $tables_excluded = Civi::settings()->get('tables_excluded');
   if (empty($tables_excluded)){
     $tables_excluded = array();
   }
+
+  $endPremature = FALSE;
   foreach($logTables as $table){
     if (!in_array($table, $tables_excluded)){
+      _logOutput('table', $table);
+
+      $dateNow = date('Y-m-d H:i:s');
+      $dateYesterday = date('Y-m-d H:i:s', strtotime('-1 days'));
+
+      $tableCompleted = CRM_Core_DAO::singleValueQuery("
+        SELECT log_id
+        FROM `{$loggingDB}`.civicrm_logretention_log
+        WHERE log_date BETWEEN '{$dateNow}' AND '{$dateYesterday}'
+          AND log_table = '{$table}'
+          AND log_completed = 1
+        ORDER BY id DESC
+        LIMIT 1
+      ");
+      if ($tableCompleted) {
+        continue;
+      }
+
+      //check logretention_log to see if we need to pick up where we left off
+      $lastLog = CRM_Core_DAO::singleValueQuery("
+        SELECT log_id
+        FROM `{$loggingDB}`.civicrm_logretention_log
+        WHERE log_date BETWEEN '{$dateYesterday}' AND '{$dateNow}'
+          AND log_table = '{$table}'
+          AND log_completed = 0
+        ORDER BY id DESC
+        LIMIT 1
+      ");
+      _logOutput('$dateNow', $dateNow);
+      _logOutput('$dateYesterday', $dateYesterday);
+      _logOutput('$lastLog', $lastLog);
+      $lastLogSql = (!empty($lastLog)) ? "AND id > {$lastLog}" : '';
+
+      //get log row IDs with at least one log outside the retention window
+      $limit = CRM_Utils_Array::value('limit', $params);
       $entity_id = "
         SELECT id
         FROM `{$loggingDB}`.$table
+        WHERE log_date < %1
+          {$lastLogSql}
         GROUP BY id
       ";
-      $entity_data = CRM_Core_DAO::executeQuery($entity_id);
+      _logOutput('entity_id', $entity_id);
+      $entity_data = CRM_Core_DAO::executeQuery($entity_id, $paramsCommon);
 
+      $i = $x = 0;
       while ($entity_data->fetch()) {
         $id = $entity_data->id;
 
@@ -83,7 +137,7 @@ function civicrm_api3_job_logretention($params) {
             AND id = $id
           LIMIT 1
         ";
-        $max_log_date = CRM_Core_DAO::singleValueQuery($daoMaxDate, $params);
+        $max_log_date = CRM_Core_DAO::singleValueQuery($daoMaxDate, $paramsCommon);
 
         if ($max_log_date) {
           $sql = "
@@ -93,7 +147,7 @@ function civicrm_api3_job_logretention($params) {
               AND id = $id
           ";
           //Civi::log()->debug('logretention', ['max_log_date' => $max_log_date, 'sql' => $sql]);
-          CRM_Core_DAO::executeQuery($sql, $params);
+          CRM_Core_DAO::executeQuery($sql, $paramsCommon);
 
           $sql = "
             DELETE FROM civicrm_log
@@ -101,10 +155,46 @@ function civicrm_api3_job_logretention($params) {
               AND modified_date <> '$max_log_date'
               AND entity_id = $id
             ";
-          CRM_Core_DAO::executeQuery($sql, $params);
+          CRM_Core_DAO::executeQuery($sql, $paramsCommon);
+
+          $i++;
+          $x++;
+          if ($i % 250 == 0) {
+            _logOutput('i', $i);
+            _storeRetentionLog($loggingDB, $table, $id);
+          }
         }
+
+        if (!empty($limit) && $x >= $limit) {
+          $endPremature = TRUE;
+          continue 2;
+        }
+      }
+
+      //if we didn't end prematurely due to limit, set completed
+      if (!$endPremature) {
+        _storeRetentionLog($loggingDB, $table, 0, 1);
       }
     }
   }
   return civicrm_api3_create_success("Deleted log entries that were older than $retentionPeriod");
+}
+
+function _logOutput($label, $var) {
+  if (defined('LOGOUTPUT') && LOGOUTPUT) {
+    CRM_Core_Error::debug_var($label, $var, TRUE, TRUE, 'logretention');
+  }
+}
+
+function _storeRetentionLog($db, $table, $id, $completed = 0) {
+  CRM_Core_DAO::executeQuery("
+    INSERT INTO `{$db}`.civicrm_logretention_log
+    (log_table, log_id, log_completed)
+    VALUES
+    (%1, %2, %3)
+  ", array(
+    1 => array($table, 'String'),
+    2 => array($id, 'Positive'),
+    3 => array($completed, 'Integer'),
+  ));
 }
